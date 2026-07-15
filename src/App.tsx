5d5b444f23a7fa01052640b8f8954660fac5e4a1
lorrana <lorranamartins961@gmail.com>
2026-07-15 20:58:55 -0300

    feat: adiciona seção de categorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Categories } from "./components/Categories"
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
 
@@ -7,7 +8,9 @@ function App() {
       <Header />
       <main className="py-10">
         <Hero />
+        <Categories />
       </main>
+
     </>
   )
 }
