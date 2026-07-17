34f3da34274b18d4712768885d150ec8f2bab2dd
lorrana <lorranamartins961@gmail.com>
2026-07-16 22:14:43 -0300

    feat: adicionada galeria do projeto

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,38 @@
+import galeriaHomem from '@/assets/images/galeria-homem.jpg'
+import galeriaModelo from '@/assets/images/galeria-modelo.jpg'
+import galeriaTenisBrancoPreto from '@/assets/images/galeria-tenis-branco-e-preto.jpg'
+import galeriaTenisCinza from '@/assets/images/galeria-tenis-cinza.jpg'
+import galeriaTenisColorido from '@/assets/images/galeria-tenis-colorido.jpg'
+import galeriaTenisRoxo from '@/assets/images/galeria-tenis-roxo.jpg'
+
+export const Gallery = () => {
+    return (
+        <section className="container mb-10">
+            <div className="gallery-grid">
+                <div className="gallery-item gallery-item-highlight rounded-[20px] overflow-hidden">
+                    <img src={galeriaHomem} alt="Homem usando tênis SyntaxWear" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="gallery-item gallery-item-white rounded-[20px] overflow-hidden">
+                    <img src={galeriaTenisBrancoPreto} alt="Tênis branco e preto SyntaxWear" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="gallery-item gallery-item-model rounded-[20px] overflow-hidden">
+                    <img src={galeriaModelo} alt="Modelo feminina usando tênis SyntaxWear" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="gallery-item gallery-item-color rounded-[20px] overflow-hidden">
+                    <img src={galeriaTenisColorido} alt="Tênis colorido SyntaxWear" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="gallery-item gallery-item-silver rounded-[20px] overflow-hidden">
+                    <img src={galeriaTenisCinza} alt="Tênis cinza SyntaxWear" className="w-full h-full object-cover" />
+                </div>
+
+                <div className="gallery-item gallery-item-purple rounded-[20px] overflow-hidden">
+                    <img src={galeriaTenisRoxo} alt="Tênis roxo SyntaxWear" className="w-full h-full object-cover" />
+                </div>
+            </div>
+        </section>
+    )
+}
